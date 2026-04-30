3c780c7bbd72d124bab819cbaea7648e374df41a
Linsong Wang <blinsowa@amazon.com>
2026-04-30 22:53:14 +0000

    new

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,24 +3,16 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+const s3 = new S3Client({ region: "us-west-2" });
+
 export async function GET() {
   try {
-    const s3 = new S3Client({
-      region: "us-west-2",
-      credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-        sessionToken: process.env.S3_SESSION_TOKEN!,
-      },
-    });
-
     const resp = await s3.send(
       new GetObjectCommand({
         Bucket: "binding-gameday-blinsowa",
         Key: "hello.txt",
       })
     );
-
     const content = await resp.Body?.transformToString();
     return NextResponse.json({ content });
   } catch (e) {
